1acc26b6b0cd733f6432d95c67062993f02ed907
rafaelUro <rafael.lima.uro@gmail.com>
2022-08-16 19:40:57 -0300

    criei os personagens e comecei a historia

---
+++ historia.js
@@ -0,0 +1,4 @@
+// era uma vez três porquinhos, os três porquinhos viviam em paz, cada um em sua casa. 
+// o porquinho mais novo tinha uma casa de palha, o do meio tinha uma casa de madeira e o mais velho tinha uma casa de tijolos.
+// em um belo dia um lobo apareceu, falando que queria devorar o porquinho mais novo, e o porquinho dizia que ele não iria, com isso o lobo assoprou, e casa do porquinho se desfez,  mas para sorte dele ele fugiu para a casa do irmão do meio.
+// então o lobo foi de novo a casa do irmão do meio e falou que ia devorar os dois, eles dizia que não,  e  o lobo assoprou a casa  
