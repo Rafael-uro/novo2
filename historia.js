f5cb84095adaab362e8e61d5623ddf3084ba8e35
rafaelUro <rafael.lima.uro@gmail.com>
2022-08-16 19:46:59 -0300

    atualizei alguns pontos da historia

--- historia.js
+++ historia.js
@@ -1,4 +1,6 @@
 // era uma vez três porquinhos, os três porquinhos viviam em paz, cada um em sua casa. 
 // o porquinho mais novo tinha uma casa de palha, o do meio tinha uma casa de madeira e o mais velho tinha uma casa de tijolos.
 // em um belo dia um lobo apareceu, falando que queria devorar o porquinho mais novo, e o porquinho dizia que ele não iria, com isso o lobo assoprou, e casa do porquinho se desfez,  mas para sorte dele ele fugiu para a casa do irmão do meio.
-// então o lobo foi de novo a casa do irmão do meio e falou que ia devorar os dois, eles dizia que não,  e  o lobo assoprou a casa  
+// então o lobo foi de novo a casa do irmão do meio e falou que ia devorar os dois, eles dizia que não,  e  o lobo assoprou a casa, teve mais dificuldade, mas derrubou-a.
+// e os dois irmãos fugiram pra casa do mais velho, e o lobo mal de novo falou que iria devora-los, e disseram que não iria.
+// então o lobo assoprou, assouprou, assoprou, mas nada acontecia, ele assutado fugiu, assim mostrando que mesmo que algo seja mais trabalhoso pode ser mais valioso
